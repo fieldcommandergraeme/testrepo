63b0817de7c797a0f049e33576df74734bc4f311
g_macadam <g_macadam@EKNBENGINEER01>
2019-04-03 16:38:54 +0100

    Add project files.

---
+++ testServiceexmos/Service1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web.Services;
+
+namespace testServiceexmos
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        Boolean IsNegativeBalance = false;
+        string Address;
+        string Name;
+        int AccountID;
+
+        //public double money { get; set; }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public double balance(double? Balance)
+        {
+            if(Balance != null)
+            {
+                return 250.00;
+            } else
+            {
+                return Convert.ToDouble(Balance);
+            }
+            
+        }
+
+        [WebMethod]
+        public static bool AddPayment(int AccountID, double money, double Balancex)
+        {
+            Service1 s = new Service1();
+            if((s.balance(Balancex) - money) <= 0)
+            {
+                s.IsNegativeBalance = true;
+            } else
+            {
+                s.IsNegativeBalance = false;
+            }
+
+            return s.IsNegativeBalance;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
